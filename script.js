0bd2bf42c79b702d13a6a1b347dc2e759c30171a
Hialii <110984478+Hialii@users.noreply.github.com>
2023-02-15 13:58:29 -0300

    finish

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const wrapper = document.querySelector(".wrapper"),
-generateBtn = wrapper.querySelector(".form button");
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener('click', () => {
-   wrapper.classList.add("active")
+   let qrValue = qrInput.value;
+   if(!qrValue) return;
+   generateBtn.textContent = "Generating QR Code..."
+   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+   qrImg.addEventListener('load', () => {
+      wrapper.classList.add("active");
+      generateBtn.textContent = "Generate QR Code"
+   });
+ 
+});
+
+qrInput.addEventListener('keyup', () => {
+   if(!qrInput.value) {
+      wrapper.classList.remove("active")
+   }
 })
